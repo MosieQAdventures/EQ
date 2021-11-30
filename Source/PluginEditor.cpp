ed8b72485e4ae4547b537a43ad2d732c17d4306f
MosieQ <moszhub@gmail.com>
2021-11-30 02:51:09 +0100

    working gui z napisami, bez skew'a 25203

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -97,23 +97,43 @@ void RotarySliderWithLabels::paint(juce::Graphics& g)
 
     //g.setColour(Colours::red);
     //g.drawRect(getLocalBounds());
-    g.setColour(Colours::white);
-    g.drawRoundedRectangle(sliderBounds.toFloat(), 2.f, 2.f);
+    //g.setColour(Colours::white);
+    //g.drawRoundedRectangle(sliderBounds.toFloat(), 2.f, 2.f);
 
     this->setColour(juce::Slider::ColourIds::rotarySliderFillColourId, Colour(165u, 56u, 56u));
     this->setColour(juce::Slider::ColourIds::thumbColourId, Colour(25u, 25u, 25u));
     this->setColour(juce::Slider::ColourIds::rotarySliderOutlineColourId, Colour(63u, 63u, 63u));
 
     getLookAndFeel().drawRotarySlider(g,
-                                      sliderBounds.getX(),
-                                      sliderBounds.getY(),
-                                      sliderBounds.getWidth(),
-                                      sliderBounds.getHeight(),
-                                      jmap(getValue(), range.getStart(), range.getEnd(), 0.0, 1.0),
-                                      startAng,
-                                      endAng,
-                                      *this);
+        sliderBounds.getX(),
+        sliderBounds.getY(),
+        sliderBounds.getWidth(),
+        sliderBounds.getHeight(),
+        jmap(getValue(), range.getStart(), range.getEnd(), 0.0, 1.0),
+        startAng,
+        endAng,
+        *this);
 
+    auto center = sliderBounds.toFloat().getCentre();
+    auto radius = sliderBounds.getWidth() * 0.5f;
+
+    Path p;
+
+    auto str = getDisplayString();
+
+    auto c = center.getPointOnCircumference(radius - getTextHeight() * 0.4, MathConstants<float>::pi);
+    
+    Rectangle<float> r;
+    r.setSize(g.getCurrentFont().getStringWidth(str), getTextBoxHeight());
+    r.setCentre(c);
+    r.setY(r.getY() + getTextHeight());
+
+    p.addRoundedRectangle(r, 2.f);
+
+    g.setColour(Colours::white);
+    g.setFont(getTextHeight());
+    g.drawFittedText(str, r.toNearestInt(), juce::Justification::centred, 1);
+    //25504
 }
 
 juce::Rectangle<int> RotarySliderWithLabels::getSliderBounds() const
@@ -131,6 +151,44 @@ juce::Rectangle<int> RotarySliderWithLabels::getSliderBounds() const
     return r;
 }
 
+
+juce::String RotarySliderWithLabels::getDisplayString() const
+{
+    if (auto* choiceParam = dynamic_cast<juce::AudioParameterChoice*>(param))
+        return choiceParam->getCurrentChoiceName();
+
+    juce::String str;
+    bool addK = false;
+
+    if (auto* floatParam = dynamic_cast<juce::AudioParameterFloat*>(param))
+    {
+        float val = getValue();
+        if (val > 999.0f)
+        {
+            val /= 1000.f;
+            addK = true;
+        }
+
+        str = juce::String(val, (addK ? 2 : 0));
+    }
+    else
+    {
+        jassertfalse;
+    }
+
+    if (suffix.isNotEmpty())
+    {
+        str << " ";
+        if (addK)
+            str << "k";
+
+        str << suffix;
+    }
+
+    return str;
+
+    //return juce::String(getValue());
+}
 //============================================================
 
 ResponseCurveComponent::ResponseCurveComponent(EQ_Hubert_MoszAudioProcessor& p) : audioProcessor(p)
@@ -183,7 +241,8 @@ void ResponseCurveComponent::timerCallback()
 void ResponseCurveComponent::paint(juce::Graphics& g)
 {
     // (Our component is opaque, so we must completely fill the background with a solid colour)
-    g.fillAll(getLookAndFeel().findColour(juce::ResizableWindow::backgroundColourId));
+    //g.fillAll(getLookAndFeel().findColour(juce::ResizableWindow::backgroundColourId));
+    g.fillAll(juce::Colour(31u, 31u, 31u));
 
     auto responseArea = getLocalBounds();
 
